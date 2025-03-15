17ac4ec6ff88fc3faa6d1a8c973624a4dd4763c8
Shaleengovil25 <shaleengovil2000@gmail.com>
2025-03-15 12:58:54 +0530

    switched to feature/camunda

--- src/main/java/com/shikhakunj/udemymicroservicestutorial/post/PostRepository.java
+++
@@ -1,7 +0,0 @@
-package com.shikhakunj.udemymicroservicestutorial.post;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PostRepository extends JpaRepository<Post, Integer>{
-
-}
--- src/main/java/com/shikhakunj/udemymicroservicestutorial/user/jpa/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.shikhakunj.udemymicroservicestutorial.user.jpa;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.shikhakunj.udemymicroservicestutorial.user.User;
-
-public interface UserRepository extends JpaRepository<User,Integer>{
-
-}
