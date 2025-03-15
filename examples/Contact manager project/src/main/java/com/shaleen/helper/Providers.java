8064efa999b4c65c5da5fc24b3065e7c9c9b47ac
Shaleengovil25 <shaleengovil2000@gmail.com>
2025-03-15 13:05:52 +0530

    refactored code

--- examples/Contact manager project/src/main/java/com/shaleen/helper/Providers.java
+++
@@ -1,5 +0,0 @@
-package com.shaleen.helper;
-
-public enum Providers {
-	SELF, GOOGLE, GITHUB
-}
--- examples/Contact manager project/src/main/java/com/shaleen/repo/ContactRepo.java
+++
@@ -1,9 +0,0 @@
-package com.shaleen.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.shaleen.entity.Contact;
-
-public interface ContactRepo extends JpaRepository<Contact, String>{
-
-}
